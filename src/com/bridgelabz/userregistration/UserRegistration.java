f59f38aa78488c3bf9b3fdfc5db082731580dfda
VighneshBilgi <vhbilgi@gmail.com>
2022-06-05 18:12:50 +0530

    Added: UC2_Last_Name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -6,8 +6,6 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    static Scanner scanner = new Scanner(System.in);
-
     public void firstName(String firstName) {
 
         String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
@@ -24,12 +22,32 @@ public class UserRegistration {
             System.out.println("First Name is not valid.");
     }
 
+    public void lastName(String lastName) {
+
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
+
+        if(result == true)
+            System.out.println("Last Name is valid.");
+        else
+            System.out.println("Last Name is not valid.");
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to User Registration Problem:");
         UserRegistration ur = new UserRegistration();
+
         System.out.println("Enter First Name:");
         String firstName = scanner.next();
         ur.firstName(firstName);
 
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+        ur.lastName(lastName);
     }
 }
